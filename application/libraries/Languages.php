2a7cb009ad7788b2c440c081dfbac1f342629ed4
Matthieu Brunet <matthieu@brunet.fr>
2016-06-23 12:35:49 +0200

    Add support to UFT-8 chars in POEditor_Extract

--- application/libraries/Languages.php
+++ application/libraries/Languages.php
@@ -160,7 +160,7 @@ class Languages {
     function POEditor_Extract() {
 	$path   = FCPATH;
 	$pofile = APPPATH.$this->config['gettext_locale_dir'] . DIRECTORY_SEPARATOR . $this->CI->config->item('domain') . '.pot';
-	$cmd = "cd /tmp ; echo '' > messages.po ; find $path -type f -iname \"*.php\" | xgettext --keyword=_ --keyword=_e -j -f - ; mv messages.po $pofile";
+	$cmd = "cd /tmp ; echo '' > messages.po ; find $path -type f -iname \"*.php\" | xgettext --from-code=" . $this->config['gettext_catalog_codeset'] . " --keyword=_ --keyword=_e -j -f - ; mv messages.po $pofile";
 	exec($cmd);
     }
     
